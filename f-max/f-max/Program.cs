2ce55b89d7c789c71553691312d87227f830d41c
Marimout <truong.quocanh@outlook.com>
2018-03-01 19:02:27 +0100

    Inputs + code for reading

--- f-max/f-max/Program.cs
+++ f-max/f-max/Program.cs
@@ -9,14 +9,54 @@ namespace f_max
 {
     class Program
     {
-        public static string inputFile = @"..\..\..\..\data\test.in";
+        public static string inputFile = @"..\..\..\..\data\a_example.in";
+
+        public static int R;
+        public static int C;
+        public static int F;
+        public static int N;
+        public static int B;
+        public static int T;
+
+        public static List<Ride> rides = new List<Ride>();
 
         static void Main(string[] args)
         {
             using (var reader = new StreamReader(inputFile))
             {
-                Console.WriteLine(reader.ReadLine());
+                var t = Array.ConvertAll(reader.ReadLine().Split(' '), Int32.Parse);
+                R = t[0];
+                C = t[1];
+                F = t[2];
+                N = t[3];
+                B = t[4];
+                T = t[5];
+
+                for (int i = 0; i < N; i++)
+                {
+                    t = Array.ConvertAll(reader.ReadLine().Split(' '), Int32.Parse);
+
+                    rides.Add(new Ride(t[0], t[1], t[2], t[3], t[4], t[5]));
+                }
             }
+        }
+    }
+
+    public class Ride
+    {
+        public int a, b;
+        public int x, y;
+        public int s;
+        public int f;
+
+        public Ride(int _a, int _b, int _x, int _y, int _s, int _f)
+        {
+            a = _a;
+            b = _b;
+            x = _x;
+            y = _y;
+            s = _s;
+            f = _f;
         }
     }
 }
