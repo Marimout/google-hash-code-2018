b303cf4bd07272d9418fa55d61dd3e5ae40dbebb
Marimout <truong.quocanh@outlook.com>
2018-03-01 18:49:32 +0100

    Hello world !

---
+++ f-max/f-max/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace f_max
+{
+    class Program
+    {
+        public static string inputFile = @"..\..\..\..\data\test.in";
+
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(inputFile))
+            {
+                Console.WriteLine(reader.ReadLine());
+            }
+        }
+    }
+}
